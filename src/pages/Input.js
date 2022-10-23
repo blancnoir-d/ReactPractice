8e00a7afc699c39e3b7284082d8d9e8c26c8113b
Saeha <blancnoir.d@gmail.com>
2022-10-23 23:23:09 +0900

    없음

--- src/pages/Input.js
+++ src/pages/Input.js
@@ -2,9 +2,11 @@ import React, {useState} from "react";
 
 const Input = () => {
     const [txtValue, setTextValue] = useState("");
+
     const onChange = (e) => { //onChange가 일어날때 마다 이벤트를(e)가져올 건데
         setTextValue(e.target.value)
     }
+
     return(
         <div>
            <input type="text" value={txtValue} onChange={onChange}/>
